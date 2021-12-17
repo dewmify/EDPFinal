29f3735df88330f675db11db5fb6a28921376445
Joshua Foo <foo.joshua55@gmail.com>
2021-12-17 16:05:24 +0800

    added course.cs and review.cs in models

---
+++ Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDPFinal.Models
+{
+    public class Course
+    {
+        public int courseID { get; set; }
+        public string courseTitle { get; set; }
+        public string courseInfo { get; set; }
+        public int coursePrice { get; set; }
+        public string courseGenre { get; set; }
+        public bool courseFormat { get; set; }
+    }
+}
