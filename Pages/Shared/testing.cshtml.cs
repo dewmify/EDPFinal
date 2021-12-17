0571600bfd07790effe903ba2ccdabafe2cb043d
Jp0622 <jiapengss22@gmail.com>
2021-12-17 15:50:39 +0800

    testingfile

---
+++ Pages/Shared/testing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EDPFinal.Pages.Shared
+{
+    public class testingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
