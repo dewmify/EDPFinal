9a92a2bd7c7b475fc76abd7947bb7e92179d68c7
Joshua Foo <foo.joshua55@gmail.com>
2021-12-17 16:22:27 +0800

    Added User.cs and Course.cs Model Attributes (unfinished?)

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,16 @@ namespace EDPFinal.Models
     public class Course
     {
         public int courseID { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Course Title cannot be longer than 100 characters.")]
         public string courseTitle { get; set; }
+        [StringLength(300, ErrorMessage ="Course Information cannot be longer than 500 characters")]
         public string courseInfo { get; set; }
+        [Required]
         public int coursePrice { get; set; }
+        [Required]
         public string courseGenre { get; set; }
+        [Required]
         public bool courseFormat { get; set; }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace EDPFinal.Models
 {
     public class User
     {
+        public int userID { get; set; }
+
+        [Required]
+        public string userEmail { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.")]
+        public string userName { get; set; }
+
     }
 }
