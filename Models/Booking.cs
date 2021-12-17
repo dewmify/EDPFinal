67ffbe4353bfdf318793bac10519a21486c5c647
Jordan <jordanotk@gmail.com>
2021-12-17 16:05:23 +0800

    Booking model

---
+++ Models/Booking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDPFinal.Models
+{
+    public class Booking
+    {
+        public int BookingID { get; set; }
+        public DateTime BookingTime { get; set; }
+        
+    }
+}
