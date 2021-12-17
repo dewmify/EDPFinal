364ed1a830b96f57fe03e29573dafb8924d67059
Jp0622 <jiapengss22@gmail.com>
2021-12-17 16:43:38 +0800

    admin cs file

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -7,5 +7,8 @@ namespace EDPFinal.Models
 {
     public class Admin
     {
+        public int adminID {get; set;}
+        public string adminName {get; set;}
+        public string adminEmail {get; set;} 
     }
 }
