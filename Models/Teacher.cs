b493e1c765fab17257c4cea0a403b9f967331109
Ryo Seah <ryoseahhx@gmail.com>
2021-12-17 16:05:36 +0800

    Created Teacher Model

---
+++ Models/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDPFinal.Models
+{
+    public class Teacher
+    {
+        public int TeacherID { get; set; }
+        public string TeacherName { get; set; }
+        public string TeacherEmail { get; set; }
+    }
+}
