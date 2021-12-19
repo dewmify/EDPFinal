ac07b571c9c6be554ec79a3b35433921bfd31aba
Jordan <jordanotk@gmail.com>
2021-12-19 14:39:19 +0800

    booking

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EDPFinal.Models
 {
     public class Booking
     {
+        [Key]
         public int BookingID { get; set; }
+
         public DateTime BookingTime { get; set; }
         //CourseID from Course model
+        [ForeignKey("")]
         public int CourseID { get; set; }
         //StudentID from Student model
+        [ForeignKey("")]
         public int StudentID { get; set; }
     }
 }
