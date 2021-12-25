a099f276e42de4d13162edc5847106d14b39749d
Jp0622 <jiapengss22@gmail.com>
2021-12-25 20:03:22 +0800

    Update on user.cs and create login.cshtml

--- Models/User.cs
+++ Models/User.cs
@@ -8,6 +8,7 @@ namespace EDPFinal.Models
 {
     public class User
     {
+        [Key]
         public int userID { get; set; }
 
         [Required, RegularExpression(@"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$", ErrorMessage = "Invalid email")]
@@ -16,12 +17,17 @@ namespace EDPFinal.Models
         [Required, StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.")]
         public string userName { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
         public string userPassword { get; set; }
+
+        [Compare("Password", ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        public string confirmPassword { get; set; }
+
         public string userPhoneNo { get; set; }
 
         [Required]
-        
         public bool registrationStatus { get; set; }
 
     }
---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EDPFinal.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
