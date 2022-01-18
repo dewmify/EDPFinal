6dca985699eecffa772912ec5d4c8a841259f239
Jp0622 <jiapengss22@gmail.com>
2022-01-18 11:38:02 +0800

    creating registration page and updating login html

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace EDPFinal.Pages
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
+            public void OnGet()
         {
         }
     }
---
+++ Pages/Registration.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EDPFinal.Pages
+{
+    public class RegistrationModel : PageModel
+    {
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string strName = Request.Form["username"];
+            string email = Request.Form["email"];
+            string pwd = Request.Form["password"];
+            string aginpwd = Request.Form["againpwd"];
+            if (!pwd.Equals(aginpwd))
+            {
+                //Response.Write("<script>alert('Password does not match')</script>");
+                return;
+            }
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
