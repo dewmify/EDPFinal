f6bd347714ded0dbc2234706fdc3816b192bf746
Ryo Seah <ryoseahhx@gmail.com>
2022-02-14 09:20:35 +0800

    Added Video Route, Content not added to video page yet

--- Pages/Teacher/UpdateCourse.cshtml.cs
+++ Pages/Teacher/UpdateCourse.cshtml.cs
@@ -29,7 +29,7 @@ namespace EDPFinal.Pages.Teacher
             {
                 return RedirectToPage("../Index");
             }
-            if (id == null)
+            if (id == 0)
             {
                 return NotFound();
             }
